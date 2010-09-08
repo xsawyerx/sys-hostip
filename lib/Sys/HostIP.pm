71b0f7ca8c0b9431e1fe54fd275eb3f35a021144
Sawyer X <xsawyerx@cpan.org>
2010-09-08 11:07:32 +0200

    fixed RT #44845

--- lib/Sys/HostIP.pm
+++ lib/Sys/HostIP.pm
@@ -142,7 +142,8 @@ sub _get_unix_interface_info {
     if ( ($line =~/^\s+/) && ($interface) ) {
       $if_info{$interface} .= $line;
     }
-    elsif (($interface) = ($line =~/(^\w+(?:\d)?(?:\:\d+)?)/)) {
+    # FIXME: refactor this regex
+    elsif (($interface) = ($line =~/(^\w+(?:\d)?(?:\.\d+)?(?:\:\d+)?)/)) {
       $line =~s/\w+\d(\:)?\s+//;
       $if_info{$interface} = $line;
     }
